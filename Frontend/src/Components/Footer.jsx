122dfcfeec4a95ddca218d24f93cb48177e28e46
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-03 22:30:31 +0530

    feat(Footer): Creating Footer Component

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            {/* Left Section */}
+            <div>
+
+            </div>
+
+            {/* Center Section */}
+            <div>
+
+            </div>
+
+            {/* Right Section */}
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
