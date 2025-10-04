c936ac3fe8a7c1577d76fc745f21f86d7874ae7b
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-05 00:26:25 +0530

    feat(Appointment): Creating doctor appointment page

--- Frontend/src/Pages/Appointment.jsx
+++ Frontend/src/Pages/Appointment.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../Context/AppContext';
+import { assets } from '../assets/assets_frontend/assets';
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams();
+  const {doctors} = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((item) => item._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [docId, doctors]);
+
+  return docInfo && (
     <div>
-        
+        {/* ---------- Doctor Information ---------- */}
+        <div>
+
+          <div>
+            <img src={docInfo?.image} alt="" />
+          </div>
+
+          <div>
+            {/* -------- Doctor Details ---------- */}
+            <p>{docInfo?.name} <img src={assets.verified_icon} alt="" /></p>
+            <div>
+              <p>{docInfo?.degree} - {docInfo?.speciality}</p>
+              <button>{docInfo?.experience}</button>
+            </div>
+            {/* -------- Doctor About ---------- */}
+            <div>
+              <p>About <img src={assets.info_icon} alt="" /></p>
+              <p>{docInfo?.about}</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
