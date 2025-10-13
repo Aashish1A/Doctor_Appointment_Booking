1a7d6b6f0f9371a2fbf07e9fd09e5d190d2ece6c
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-13 23:21:03 +0530

    feat(AppContext): Added currency symbol

--- Frontend/src/Context/AppContext.jsx
+++ Frontend/src/Context/AppContext.jsx
@@ -5,10 +5,11 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
-    
+  const currency = '$';
 
   const value = {
     doctors,
+    currency,
   };
 
   return (
