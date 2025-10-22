e66d74c00111b2e028ac895de94523ff5970c126
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-23 01:54:47 +0530

    feat(mongodb): Created connectDB function to connect with database

---
+++ Backend/config/mongodb.js
@@ -0,0 +1,15 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        });
+        await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+}
+
+export default connectDB;
