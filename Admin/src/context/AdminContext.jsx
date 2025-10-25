355891fba32477d8fadb001e941422127c7f97ba
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-25 06:34:10 +0530

    feat(AdminContext): Added getAllDoctors function

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -1,25 +1,42 @@
+import axios from "axios";
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = ({ children }) => {
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+  const [doctors, setDoctors] = useState([]);
 
-    const [aToken, setAToken] = useState(localStorage.getItem("aToken")? localStorage.getItem("aToken") : "");
+  const backendURL = import.meta.env.VITE_BACKEND_URL;
 
-    const backendURL = import.meta.env.VITE_BACKEND_URL;
-
-    const value = {
-        aToken,
-        setAToken,
-        backendURL
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.get(`${backendURL}/api/admin/get-all-doctors`,{headers: {atoken: aToken},});
+      if (data.success) {
+        setDoctors(data.data);
+        console.log(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
+  const value = {
+    aToken,
+    setAToken,
+    backendURL,
+    getAllDoctors,
+    doctors,
+  };
 
-    return (
-        <AdminContext.Provider value={value}>
-            {children}
-        </AdminContext.Provider>
-    );
+  return (
+    <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
+  );
 };
 
 export default AdminContextProvider;
