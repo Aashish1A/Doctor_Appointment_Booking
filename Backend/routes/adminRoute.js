286ad332d7b7330f676a68b3f99555039b0d7174
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-31 20:25:55 +0530

    feat(adminRoute): Created multiple routes for admin

---
+++ Backend/routes/adminRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { addDoctor, adminLogin, getAllDoctors } from '../controllers/adminController.js';
+import upload from '../middlewares/multer.js';
+import { authAdmin } from '../middlewares/authAdmin.js';
+
+const adminRouter = express.Router();
+
+adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
+adminRouter.post('/admin-login', adminLogin);
+adminRouter.get('/get-all-doctors', authAdmin, getAllDoctors);
+
+export default adminRouter;
