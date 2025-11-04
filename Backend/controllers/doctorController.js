45e9f43959b4e40cc5b7996db052da1661296002
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-11-04 23:12:23 +0530

    feat(doctorController): Building API

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -2,7 +2,8 @@
 
 export const changeAvailability = async(req, res) => {
   try {
-   
+    
+    res.send({success: true, message: "Availability status changed", data: doctor });
   } catch (error) {
     console.log(error);
     res.send({success: false, message: error.message });
