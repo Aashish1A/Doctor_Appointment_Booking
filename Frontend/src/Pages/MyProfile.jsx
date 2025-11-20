6a07809c12f2b17a669065be9701d9e5c4220222
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-11-20 17:22:08 +0530

    feat(MyProfile): Completed MyProfile page

--- Frontend/src/Pages/MyProfile.jsx
+++ Frontend/src/Pages/MyProfile.jsx
@@ -1,26 +1,61 @@
 import React, { useState } from 'react'
+import { useContext } from 'react';
+import { AppContext } from '../Context/AppContext';
 import { assets } from '../assets/assets_frontend/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const MyProfile = () => {
 
-  const [userData, setUserData] = useState({
-    name: "John Doe",
-    image: assets.profile_pic,
-    email: 'john.doe@example.com',
-    phone: '+1 234 567 890',
-    address: {
-      line1: '123 Main St',
-      line2: 'Apt 4B',
-    },
-    gender:"Male",
-    dob: '1990-01-01',
-  });
+  const { userData, setUserData, token, backendURL, loadUserProfileData } = useContext(AppContext);
 
   const [editMode, setEditMode] = useState(false);
+  const [image, setImage] = useState(false);
 
-  return (
+  const updateUserProfileData = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("name", userData.name);
+      formData.append("phone", userData.phone);
+      formData.append("address", JSON.stringify(userData.address));
+      formData.append("gender", userData.gender);
+      formData.append("dob", userData.dob);
+      if (image) {
+        formData.append("image", image);
+      }
+
+      const {data} = await axios.post(`${backendURL}/api/user/update-profile`, formData, {headers: {token}});
+      if(data.success){
+        toast.success(data.message);
+        await loadUserProfileData();
+        setEditMode(false);
+        setImage(false);
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      toast.error(error.message);
+    }
+  }
+
+  return userData && (
     <div className='max-w-lg flex flex-col gap-2 text-sm'>
-        <img className='w-36 rounded' src={userData.image} alt={userData.name} />
+
+        {
+          editMode ? (
+            <label htmlFor="image">
+              <div className='inline-block relative cursor-pointer'>
+                <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image) : userData.image} alt="" />
+                <img className='w-10 absolute bottom-12 right-12' src={image ? "" : assets.upload_icon} alt="" />
+              </div>
+              <input type="file" id="image" onChange={(e) => setImage(e.target.files[0])} hidden/>
+            </label>
+          ) : (
+            <img className='w-36 rounded' src={userData.image} alt={userData.name} />
+          )
+        }
+
         {
           editMode ? (
             <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type='text' value={userData.name} onChange={(e) => setUserData({ ...userData, name: e.target.value })} />
@@ -83,7 +118,7 @@ const MyProfile = () => {
 
         <div className='mt-10'>
           { editMode ? (
-            <button className='border border-[rgb(95,111,255)] px-8 py-2 rounded-full hover:bg-[rgb(95,111,255)] hover:text-white transition-all' onClick={() => setEditMode(false)}>Save Information</button>
+            <button className='border border-[rgb(95,111,255)] px-8 py-2 rounded-full hover:bg-[rgb(95,111,255)] hover:text-white transition-all' onClick={updateUserProfileData}>Save Information</button>
           ) : (
             <button className='border border-[rgb(95,111,255)] px-8 py-2 rounded-full hover:bg-[rgb(95,111,255)] hover:text-white transition-all' onClick={() => setEditMode(true)}>Edit Profile</button>
           )}
