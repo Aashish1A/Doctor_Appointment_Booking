e3f1bf62a1e12879f2de7776f124178b586ad8af
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-11-23 23:44:42 +0530

    feat(adminRoute): Added add-doctor,admin-login,getAllDoctors and change availability routes

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { addDoctor, adminLogin, getAllDoctors } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import { authAdmin } from '../middlewares/authAdmin.js';
+import { changeAvailability } from '../controllers/doctorController.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
 adminRouter.post('/admin-login', adminLogin);
 adminRouter.get('/get-all-doctors', authAdmin, getAllDoctors);
+adminRouter.post('/change-availability', authAdmin, changeAvailability);
 
 export default adminRouter;
