fe5f69d1b406280f6d202973949e8d4291c348ab
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-11-24 20:31:41 +0530

    feat(userRoute): Create user  register,login,get-profile,book-appointment

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { bookAppointment, getUserInfo, loginUser, registerUser, updateUserProfile } from "../controllers/userController.js";
+import { authUser } from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
+
+// Routes
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+
+userRouter.get("/get-profile", authUser, getUserInfo);
+userRouter.post("/update-profile", upload.single("image"), authUser, updateUserProfile);
+
+userRouter.post("/book-appointment", authUser, bookAppointment);
+
+export default userRouter;
