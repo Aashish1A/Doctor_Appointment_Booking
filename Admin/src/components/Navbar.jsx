b683518b31477cf79cd743eeddc8111a06fd74d1
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-12-15 01:54:51 +0530

    feat(Navbar): Add Doctor context for handling doctor logout functionality

--- Admin/src/components/Navbar.jsx
+++ Admin/src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import { useNavigate } from 'react-router-dom';
+import { DoctorContext } from '../context/DoctorContext';
 
 const Navbar = () => {
 
     const {aToken, setAToken} = useContext(AdminContext);
+    const {dToken, setDToken} = useContext(DoctorContext); 
 
     const navigate = useNavigate();
 
@@ -14,6 +16,10 @@ const Navbar = () => {
           localStorage.removeItem("aToken");
           setAToken("");
        }
+        if(dToken){
+          localStorage.removeItem("dToken");
+          setDToken("");
+        }
        navigate("/");
     }
 
