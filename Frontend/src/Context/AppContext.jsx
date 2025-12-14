7ccc0d8017a83fa92fb7fdf3e3fb722907e3f9ff
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-12-15 01:54:19 +0530

    feat(AppContext): Enhance context provider with doctor data retrieval and user profile loading

--- Frontend/src/Context/AppContext.jsx
+++ Frontend/src/Context/AppContext.jsx
@@ -1,15 +1,69 @@
 import { createContext } from "react";
-import { doctors } from "../assets/assets_frontend/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
   const currency = '$';
+  const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+  const [userData, setUserData] = useState(false);
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(`${backendURL}/api/doctor/list`);
+      if (data.success) {
+        setDoctors(data.doctors);
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(`${backendURL}/api/user/get-profile`, {headers: {token}});
+      if (data.success) {
+        setUserData(data.user);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  useEffect(() => {
+    if(token){
+      loadUserProfileData();
+    }else{
+      setUserData(false);
+    }
+  }, [token]);
 
   const value = {
-    doctors,
+    doctors,getDoctorsData,
     currency,
+    backendURL,
+    token,setToken,
+    userData,setUserData,
+    loadUserProfileData,
   };
 
   return (
