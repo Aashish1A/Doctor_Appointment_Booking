387d24a920aeeeb382b3f465fbb01443b42debf6
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2026-01-26 22:53:29 +0530

    Update assets: Replace header image, add hero doctor background, modify logo, and update verified icon colors

--- Frontend/src/assets/assets_frontend/assets.js
+++ Frontend/src/assets/assets_frontend/assets.js
@@ -1,5 +1,6 @@
 import appointment_img from "./appointment_img.png";
 import header_img from "./header_img.png";
+import heroDoctorBg from "./heroDoctorBg.png";
 import group_profiles from "./group_profiles.png";
 import profile_pic from "./profile_pic.png";
 import contact_image from "./contact_image.png";
@@ -40,6 +41,7 @@ import Pediatricians from "./Pediatricians.svg";
 export const assets = {
   appointment_img,
   header_img,
+  heroDoctorBg,
   group_profiles,
   logo,
   chats_icon,
